a8f05caf1d61b716e56883c94107044bfb770aee
João Victor Mazetti Borges <jvmazettib@gmail.com>
2023-09-05 19:49:51 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListneer('DOMContentLoeaded', function(){
+	document.getElementById('form-sorteador')addEventListneer('submit', function(evento){
+		evento.preventDefault();
+		let numeroMaximo = document.getElementById('numero-maximo').value;
+		numeroMaximo = parseInt(numeroMaximo);
+
+		let numeroAleatorio = Math.random() * numeroMaximo;
+		numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
+		document.getElementById('resultado-valor').innerText = numeroAleatorio;
+		document.querySelector('.resultado').style.display = 'block';
+	})
+})
